0993d55e345715c62976a1df307af11dee8889ec
Todd Sampson <tsampson@cloudspace.com>
2015-11-30 11:23:54 -0500

    Adding -y flags to apt-get installs

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,10 @@ set -e
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
 sudo apt-get update
-sudo apt-get install ros-indigo-desktop-full
+sudo apt-get -y install ros-indigo-desktop-full
 sudo rosdep init
 rosdep update
-sudo apt-get install python-rosinstall
+sudo apt-get -y install python-rosinstall
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
 catkin_init_workspace

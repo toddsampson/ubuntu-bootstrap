d0883a1e7f360a4dde1e29d8619b5e828ce13437
Todd Sampson <tsampson@cloudspace.com>
2015-11-29 19:43:22 -0500

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+# Setup ROS Indigo
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
+sudo apt-get update
+sudo apt-get install ros-indigo-desktop-full
+sudo rosdep init
+rosdep update
+sudo apt-get install python-rosinstall
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+catkin_init_workspace
+cd ~/catkin_ws/
+catkin_make
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+# Setup Docker Engine and Docker Compose
+curl -sSL https://get.docker.com/ | sh
+sudo su
+curl -L https://github.com/docker/compose/releases/download/1.5.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+exit
